8623c4066d105db83b816a63d01537544ce3bdd1
Nagisa <tzr.schicksal@gmail.com>
2018-04-17 18:44:09 +0000

    sidebar 固定

--- db/migrate/20180417173350_remove_bday_year_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveBdayYearFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :bday_year, :date
-    remove_column :users, :bday_month, :date
-    remove_column :users, :bday_day, :date
-  end
-end
