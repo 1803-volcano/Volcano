3c9a6c997c3c50306eefdb249fe7cfbbd227f1a4
masayukiohira <njs4ever1988@gmail.com>
2018-04-18 02:31:04 +0000

    admins-top-modified

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -37,26 +37,4 @@ private
 end
 
 
-create_table "products", force: :cascade do |t|
-    t.string "cd_title"
-    t.string "artist"
-    t.text "picture_id"
-    t.text "sound_source"
-    t.integer "price"
-    t.string "label"
-    t.string "genre"
-    t.integer "stock"
-    t.date "start_date"
-    t.boolean "product_flg"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
-
-
-
-
-
-
-
-
 
