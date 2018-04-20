4df458d27edfdbc33ccf912e86e91e128baf47bd
Nagisa <tzr.schicksal@gmail.com>
2018-04-20 11:31:27 +0000

    favorit

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,9 @@ class Product < ApplicationRecord
   has_many :discs
   has_many :receipts
   has_many :browsings
+  has_many :users, through: :browsings
   has_many :favorites
+  has_many :users, through: :favorites
   accepts_nested_attributes_for :discs
 
   attachment :picture
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
 
   has_many :carts
   has_many :purchasers
-  has_many :browsings
-  has_many :favorites
+  has_many :browsings, dependent: :destroy #関連付くイベントが削除されると中間テーブルも削除される
+  has_many :products, through: :browsings #中間テーブルの紐付け先を呼び出す
+  has_many :favorites, dependent: :destroy #関連付くイベントが削除されると中間テーブルも削除される
+  has_many :products, through: :favorites #中間テーブルの紐付け先を呼び出す
 
 end
