8bb86dae357d2e1aa5d55aa629895cad8eb8b46b
Nagisa <tzr.schicksal@gmail.com>
2018-04-25 08:13:02 +0000

    admin user

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -8,7 +8,24 @@ class Admins::UsersController < ApplicationController
  	@user = User.find(params[:id])
  end
 
+ def edit
+ 	@user = User.find(params[:id])
+ end
+
+ def update
+ 	@user = User.find(params[:id])
+ 	@user.update(user_params)
+ 	redirect_to admins_users_path(admin_id: current_admin.id, user_id: @user.id)
+
+ end
+
  def delete
+ end
+
+ private
+
+ def user_params
+ 	params.require(:user).permit(:email, :name_sei, :name_mei, :name_kana_sei, :name_kana_mei, :birthday, :sex, :postal_code, :region, :street, :phone, :credit_card, :customer_flg)
  end
 
 end
