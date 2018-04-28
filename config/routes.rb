4fb685cf971d224ea96ff8f5128846dc2158d257
masayukiohira <njs4ever1988@gmail.com>
2018-04-28 07:42:36 +0000

    contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+    resources :contacts
+
   devise_for :admins, :controllers => {
     :registrations => 'admins/registrations',
     :sessions => 'admins/sessions',
